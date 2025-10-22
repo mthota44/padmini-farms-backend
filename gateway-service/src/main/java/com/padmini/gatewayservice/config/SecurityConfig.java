68ff1aaa1595742f9dd92741ae2b992a1e60a98a
mthota44 <manojkumarthota44@gmail.com>
2025-10-22 23:38:10 +0530

    add key clock configurations 0.0.2

--- gateway-service/src/main/java/com/padmini/gatewayservice/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/padmini/gatewayservice/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -23,11 +25,17 @@ public class SecurityConfig {
                 )
 
                 // Resource server (JWT) authentication
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
 
                 // Enable OAuth2 login (PKCE or browser-based login flow)
-                .oauth2Login(oauth2 -> {});
+                //.oauth2Login(oauth2 -> {});
 
         return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        String jwkSetUri = "http://padmini-keycloak:8090/realms/padmini-farms/protocol/openid-connect/certs";
+        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
 }
